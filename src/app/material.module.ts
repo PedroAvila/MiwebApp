ba354d3ebd38319d49a9a400eca5820db7913b0a
PedroAvila <minimainor_austin@hotmail.com>
2021-08-07 06:11:28 -0500

    Crear Formulario con Angular y Material Desing

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,10 +2,12 @@
 import { NgModule } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
+import {MatInputModule} from '@angular/material/input';
+import {MatFormField, MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
-  imports: [MatButtonModule, MatIconModule],
-  exports: [MatButtonModule, MatIconModule]
+  imports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule],
+  exports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule]
 })
 
 
