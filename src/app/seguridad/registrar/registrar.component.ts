4fc98bec93d0bf9e43ba6054029839bea575cf86
PedroAvila <minimainor_austin@hotmail.com>
2021-08-07 11:12:23 -0500

    Agregar boton submit a Form

--- src/app/seguridad/registrar/registrar.component.ts
+++ src/app/seguridad/registrar/registrar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-registrar',
@@ -10,6 +11,11 @@ export class RegistrarComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  registrarUsuario(form: NgForm){
+    console.log(form);
   }
 
 }
